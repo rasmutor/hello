59bd4dd1bfebb92ab2c50cb0e3cabf9a34f41806
Torben N. Rasmussen <Torben.Rasmussen@visma.com>
2016-09-18 18:24:04 +0200

    Change test

--- src/test/java/hello/HelloTest.java
+++ src/test/java/hello/HelloTest.java
@@ -1,9 +1,11 @@
 package hello;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class HelloTest {
     @Test
     public void test1() throws Exception {
+        Assert.assertTrue(false);
     }
 }
